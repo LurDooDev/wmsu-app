b2405e17c6b758955aa0066dbbde549d4777ed7f
Yongbi21 <111903577+Yongbi21@users.noreply.github.com>
2023-12-09 23:48:59 +0800

    laravue
    setup laravue with tailwind

---
+++ wmsu-collection-system/resources/js/components/Example.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h2>Hello from Vue!</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.');
+        }
+    }
+</script>
+
+<style scoped>
+</style>
