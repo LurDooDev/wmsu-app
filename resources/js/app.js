7eaaa90e3ae36a07fcc2240db00d58c6eaa96a4d
AndreMickael <111483607+AndreMickael@users.noreply.github.com>
2023-12-24 01:57:19 +0800

    dasdsa

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,27 +1,8 @@
 import './bootstrap';
+import 'flowbite'
 
 import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
 
 Alpine.start();
-
-
-  document.addEventListener('DOMContentLoaded', function () {
-    const dropdownButton = document.querySelector('[data-collapse-toggle="dropdown-example"]');
-    const dropdownList = document.getElementById('button');
-
-    dropdownButton.addEventListener('click', function () {
-      dropdownList.classList.toggle('hidden');
-    });
-  });
-
-
-  document.addEventListener('DOMContentLoaded', function () {
-    const hamburgerBtn = document.getElementById('hamburger-btn');
-    const navLinks = document.getElementById('nav-links');
-
-    hamburgerBtn.addEventListener('click', function () {
-      navLinks.classList.toggle('hidden');
-    });
-  });
