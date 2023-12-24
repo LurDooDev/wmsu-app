584fdf8fbe5c07b12efeac6129dc1f593de3d28d
Yongbi21 <111903577+Yongbi21@users.noreply.github.com>
2023-12-24 22:28:51 +0800

    college-semi-done

--- resources/views/colleges/index.blade.php
+++ resources/views/colleges/index.blade.php
@@ -2,8 +2,177 @@
 
     @include('layouts.sidebar')
 
-    <main class="p-4 sm:ml-64 pt-20 sm:pt-4 h-auto">
+    <main class="p-9 sm:ml-64 pt-20 sm:pt-8 h-auto">
+    <section class="bg-gray-50 dark:bg-gray-900 p-3 sm:p-5">
+        <div class="p-12 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
 
+    <div class="mx-5 px-3 lg:px-4">
+        <!-- Start coding here -->
+        <div class="bg-white dark:bg-gray-800 relative shadow-md sm:rounded-lg overflow-hidden">
+            <div class="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4">
+
+                <div class="w-full md:w-3/3 flex flex-col md:flex-row space-y-2 md:space-y-0 items-stretch md:items-center justify-end md:space-x-3 flex-shrink-0">
+                <button data-modal-target="crud-modal" data-modal-toggle="crud-modal" class="flex items-center justify-center px-4 py-2 text-sm font-medium text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">
+                        <svg class="h-3.5 w-3.5 mr-2" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                            <path clip-rule="evenodd" fill-rule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" />
+                        </svg>
+                        Add College
+                    </button>
+                </div>
+            </div>
+            <div class="overflow-x-auto">
+                <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                    <thead class="text-md text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                        <tr>
+                            <th scope="col" class="px-4 py-3">#</th>
+                            <th scope="col" class="px-4 py-3">College Code</th>
+                            <th scope="col" class="px-4 py-3">College Name</th>
+                            <th scope="col" class="px-4 py-3">Actions </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr class="border-b dark:border-gray-700">
+                            <td class="px-4 py-3">1</td>
+                            <td class="px-4 py-3">CCS</td>
+                            <td class="px-4 py-3">College of Computing Studies</td>
+                            <td class="px-8 py-2">
+                            <button title="View">
+                            <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 14">
+    <g stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+      <path d="M10 10a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
+      <path d="M10 13c4.97 0 9-2.686 9-6s-4.03-6-9-6-9 2.686-9 6 4.03 6 9 6Z"/>
+               </g>
+               </svg>
+                </button>
+                    </td>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr class="border-b dark:border-gray-700">
+                            <td class="px-4 py-3">2</td>
+                            <td class="px-4 py-3">COE</td>
+                            <td class="px-4 py-3">College of Engineering</td>
+                            <td class="px-8 py-2">
+                            <button title="View">
+                            <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 14">
+    <g stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+      <path d="M10 10a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
+      <path d="M10 13c4.97 0 9-2.686 9-6s-4.03-6-9-6-9 2.686-9 6 4.03 6 9 6Z"/>
+               </g>
+               </svg>
+                </button>
+                    </td>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr class="border-b dark:border-gray-700">
+                            <td class="px-4 py-3">3</td>
+                            <td class="px-4 py-3">CLA</td>
+                            <td class="px-4 py-3">College of Liberal Arts</td>
+                            <td class="px-8 py-2">
+                            <button title="View">
+                            <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 14">
+    <g stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+      <path d="M10 10a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
+      <path d="M10 13c4.97 0 9-2.686 9-6s-4.03-6-9-6-9 2.686-9 6 4.03 6 9 6Z"/>
+               </g>
+               </svg>
+                </button>
+                    </td>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <nav class="flex flex-col md:flex-row justify-between items-start md:items-center space-y-3 md:space-y-0 p-4" aria-label="Table navigation">
+                <span class="text-sm font-normal text-gray-500 dark:text-gray-400">
+                    Showing
+                    <span class="font-semibold text-gray-900 dark:text-white">1-10</span>
+                    of
+                    <span class="font-semibold text-gray-900 dark:text-white">1000</span>
+                </span>
+                <ul class="inline-flex items-stretch -space-x-px">
+                    <li>
+                        <a href="#" class="flex items-center justify-center h-full py-1.5 px-3 ml-0 text-gray-500 bg-white rounded-l-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                            <span class="sr-only">Previous</span>
+                            <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd" />
+                            </svg>
+                        </a>
+                    </li>
+                    <li>
+         <a href="#" aria-current="page" class="flex items-center justify-center text-sm py-2 px-3 leading-tight text-red-600 bg-primary-50 border border-red-300 hover:bg-red-100 hover:text-red-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white">1</a>
+                    </li>
+    <li>
+        <a href="#" class="flex items-center justify-center text-sm py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">2</a>
+    </li>
+    <li>
+        <a href="#" class="flex items-center justify-center text-sm z-10 py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">3</a>
+    </li>
+    <li>
+        <a href="#" class="flex items-center justify-center text-sm py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">...</a>
+    </li>
+    <li>
+        <a href="#" class="flex items-center justify-center text-sm py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">100</a>
+    </li>
+    <li>
+        <a href="#" class="flex items-center justify-center h-full py-1.5 px-3 leading-tight text-gray-500 bg-white rounded-r-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+             <span class="sr-only">Next</span>
+            <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
+            </svg>
+        </a>
+    </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+    </section>
+
+  <div id="crud-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+    <div class="relative p-4 w-full max-w-xl max-h-full">
+        <!-- Modal content -->
+        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <!-- Modal header -->
+            <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+                    Add College
+                </h3>
+                <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="crud-modal">
+                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                    </svg>
+                    <span class="sr-only">Close modal</span>
+                </button>
+            </div>
+            <!-- Modal body -->
+            <form class="p-7 md:p-5">
+                <div class="grid gap-4 mb-4 grid-cols-2">
+                    <div class="col-span-6">
+                        <label for="snumber" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">College Name</label>
+                        <input type="snumber" name="snumber" id="snumber" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
+                         focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400
+                          dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="College Of Computing Studies" required="">
+                          <span class="text-sm font-thin text-gray-500 dark:text-gray-400">Enter the full name of the college</span>
+                        </div>
+                    <div class="col-span-6">
+                        <label for="fname" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">College Code</label>
+                        <input type="fname" name="fname" id="fname" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-
+                         focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400
+                          dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="CCS" required="">
+                          <span class="text-sm font-thin text-gray-500 dark:text-gray-400">Enter the code for the college  </span>
+
+                    </div>
+                </div>
+                <button type="submit" class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none  focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    Add College
+                </button>
+            </form>
+        </div>
+    </div>
+</div>
+</div>
     </main>
 
 @include('layouts.footer')
--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -4,6 +4,8 @@
 
     <main class="p-9 sm:ml-64 pt-20 sm:pt-8 h-auto">
     <section class="bg-gray-50 dark:bg-gray-900 p-3 sm:p-5">
+        <div class="p-12 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
+
     <div class="mx-5 px-3 lg:px-4">
         <!-- Start coding here -->
         <div class="bg-white dark:bg-gray-800 relative shadow-md sm:rounded-lg overflow-hidden">
@@ -186,7 +188,7 @@
     </section>
 
   <div id="crud-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
-    <div class="relative p-4 w-full max-w-md max-h-full">
+    <div class="relative p-4 w-full max-w-xl max-h-full">
         <!-- Modal content -->
         <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
             <!-- Modal header -->
@@ -254,7 +256,8 @@
             </form>
         </div>
     </div>
-</div> 
+</div>
+</div>
     </main>
 
 @include('layouts.footer')
