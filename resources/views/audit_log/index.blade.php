9ba7735d4c29204e72e14107b7a82a0d6a74929c
Yongbi21 <111903577+Yongbi21@users.noreply.github.com>
2023-12-24 22:00:58 +0800

    audit-log done

--- resources/views/audit_log/index.blade.php
+++ resources/views/audit_log/index.blade.php
@@ -3,6 +3,92 @@
     @include('layouts.sidebar')
 
     <main class="p-4 sm:ml-64 pt-20 sm:pt-4 h-auto">
+        <div class="p-12 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
+
+<ol class="relative border-s border-gray-200 dark:border-gray-700">                  
+    <li class="mb-10 ms-7">            
+        <span class="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -start-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+        </span>
+        <div class="items-center justify-between p-4 bg-white border border-gray-200 rounded-lg shadow-sm sm:flex dark:bg-gray-700 dark:border-gray-600">
+            <time class="mb-1 text-xs font-normal text-gray-400 sm:order-last sm:mb-0">just now</time>
+            <div class="text-sm font-normal text-gray-500 dark:text-gray-300">Bonnie login from the system <a href="#" class="font-semibold text-blue-600 dark:text-blue-500 hover:underline">as an officer</a>
+    </div>
+        </div>
+    </li>
+    <li class="mb-10 ms-7">            
+        <span class="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -start-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+        </span>
+        <div class="items-center justify-between p-4 bg-white border border-gray-200 rounded-lg shadow-sm sm:flex dark:bg-gray-700 dark:border-gray-600">
+            <time class="mb-1 text-xs font-normal text-gray-400 sm:order-last sm:mb-0">just now</time>
+            <div class="text-sm font-normal text-gray-500 dark:text-gray-300">Bonnie logout from the system <a href="#" class="font-semibold text-blue-600 dark:text-blue-500 hover:underline">as an officer</a>
+    </div>
+        </div>
+    </li>
+    <li class="mb-10 ms-7">            
+        <span class="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -start-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+        </span>
+        <div class="items-center justify-between p-4 bg-white border border-gray-200 rounded-lg shadow-sm sm:flex dark:bg-gray-700 dark:border-gray-600">
+            <time class="mb-1 text-xs font-normal text-gray-400 sm:order-last sm:mb-0">1 minute ago</time>
+            <div class="text-sm font-normal text-gray-500 dark:text-gray-300">Bonnie login from the system <a href="#" class="font-semibold text-blue-600 dark:text-blue-500 hover:underline">as an officer</a>
+    </div>
+        </div>
+    </li>
+    <li class="mb-10 ms-7">            
+        <span class="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -start-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+        </span>
+        <div class="items-center justify-between p-4 bg-white border border-gray-200 rounded-lg shadow-sm sm:flex dark:bg-gray-700 dark:border-gray-600">
+            <time class="mb-1 text-xs font-normal text-gray-400 sm:order-last sm:mb-0">2 minute ago </time>
+            <div class="text-sm font-normal text-gray-500 dark:text-gray-300">Bonnie logout from the system <a href="#" class="font-semibold text-blue-600 dark:text-blue-500 hover:underline">as an officer</a>
+    </div>
+        </div>
+    </li>
+</ol>
+
+
+    <nav class="flex flex-col md:flex-row justify-between items-start md:items-center space-y-3 md:space-y-0 p-4" aria-label="Table navigation">
+        <span class="text-sm font-normal text-gray-500 dark:text-gray-400">
+            Showing
+            <span class="font-semibold text-gray-900 dark:text-white">1-10</span>
+            of
+            <span class="font-semibold text-gray-900 dark:text-white">1000</span>
+        </span>
+        <ul class="inline-flex items-stretch -space-x-px">
+            <li>
+                <a href="#" class="flex items-center justify-center h-full py-1.5 px-3 ml-0 text-gray-500 bg-white rounded-l-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                    <span class="sr-only">Previous</span>
+                    <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd" />
+                    </svg>
+                </a>
+            </li>
+            <li>
+<a href="#" aria-current="page" class="flex items-center justify-center text-sm py-2 px-3 leading-tight text-red-600 bg-primary-50 border border-red-300 hover:bg-red-100 hover:text-red-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white">1</a>
+</li>
+<li>
+<a href="#" class="flex items-center justify-center text-sm py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">2</a>
+</li>
+<li>
+<a href="#" class="flex items-center justify-center text-sm z-10 py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">3</a>
+</li>
+<li>
+<a href="#" class="flex items-center justify-center text-sm py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">...</a>
+</li>
+<li>
+<a href="#" class="flex items-center justify-center text-sm py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">100</a>
+</li>
+<li>
+<a href="#" class="flex items-center justify-center h-full py-1.5 px-3 leading-tight text-gray-500 bg-white rounded-r-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+     <span class="sr-only">Next</span>
+    <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+        <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
+    </svg>
+</a>
+</li>
+        </ul>
+    </nav>
+
+  
+</div>
 
     </main>
 
