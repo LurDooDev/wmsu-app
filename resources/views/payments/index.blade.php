bc9e47584be8343a7a28f51d5d3dba1a278cf872
AndreMickael <111483607+AndreMickael@users.noreply.github.com>
2024-01-26 21:07:05 +0800

    dasdas

--- resources/views/payments/index.blade.php
+++ resources/views/payments/index.blade.php
@@ -1,88 +1,98 @@
 @include('layouts.header')
 
-    @include('layouts.sidebar')
+@include('layouts.sidebar')
 
-    <!--Main-->
-    <main class="p-9 sm:ml-64 pt-20 sm:pt-4 h-auto">
+<!-- Main -->
+<main class="p-9 sm:ml-64 pt-20 sm:pt-4 h-auto">
     <section class="bg-gray-50 dark:bg-gray-900 p-3 sm:p-1">
-
-    <div class="mx-5 px-3 ">
-        <!-- Start coding here -->
-        <div class="bg-white dark:bg-gray-800 relative shadow-md sm:rounded-lg overflow-hidden">
-            <!-- Breadcrumb -->
-            <nav class="flex px-5 py-3 text-gray-700" aria-label="Breadcrumb">
-                <ol class="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
-                    <li class="inline-flex items-center">
-                        <span class="inline-flex items-center text-sm font-medium text-gray-700 dark:text-gray-400">
-                            <svg class="w-3 h-3 me-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                            <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z"/>
-                            </svg>
-                            Home
-                        </span>
-                    </li>
-                    <li aria-current="page">
-                        <div class="flex items-center">
-                            <svg class="rtl:rotate-180  w-3 h-3 mx-1 text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
-                            </svg>
-                            <span class="ms-1 text-sm font-medium text-gray-500 md:ms-2 dark:text-gray-400">Student Information</span>
-                        </div>
-                    </li>
-                </ol>
-            </nav>
-            <!--End Breadcrumb -->
-       <!--Table Header -->
-<div class="flex flex-col md:flex-row items-center justify-center space-y-3 md:space-y-0 md:space-x-4 p-4">
-    <div class="w-full md:w-4/6">
-        <form class="flex items-center">
-            <label for="simple-search" class="sr-only">Search</label>
-            <div class="relative w-full">
-                <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                    <svg aria-hidden="true" class="w-4 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
-                    </svg>
+        <div class="mx-5 px-3">
+            <!-- Start coding here -->
+            <div class="bg-white dark:bg-gray-800 relative shadow-md sm:rounded-lg overflow-hidden">
+                <!-- Breadcrumb -->
+                <nav class="flex px-5 py-3 text-gray-700" aria-label="Breadcrumb">
+                    <ol class="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
+                        <li class="inline-flex items-center">
+                            <span class="inline-flex items-center text-sm font-medium text-gray-700 dark:text-gray-400">
+                                <svg class="w-3 h-3 me-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                    <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z"/>
+                                </svg>
+                                Home
+                            </span>
+                        </li>
+                        <li aria-current="page">
+                            <div class="flex items-center">
+                                <svg class="rtl:rotate-180 w-3 h-3 mx-1 text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
+                                </svg>
+                                <span class="ms-1 text-sm font-medium text-gray-500 md:ms-2 dark:text-gray-400">Student Information</span>
+                            </div>
+                        </li>
+                    </ol>
+                </nav>
+                <!-- End Breadcrumb -->
+                <!-- Table Header -->
+                <div class="flex flex-col md:flex-row items-center justify-center space-y-3 md:space-y-0 md:space-x-4 p-4">
+                    <div class="w-full md:w-4/6">
+                        <form class="flex items-center">
+                            <label for="simple-search" class="sr-only">Search</label>
+                            <div class="relative w-full">
+                                <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                                    <svg aria-hidden="true" class="w-4 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                        <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
+                                    </svg>
+                                </div>
+                                <input type="text" id="simple-search" class="bg-gray-50 border border-gray-300 text-gray-900 text-lg rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 p-3 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 h-11" placeholder="Search" required="">
+                            </div>
+                        </form>
+                    </div>
                 </div>
-                <input type="text" id="simple-search" class="bg-gray-50 border border-gray-300 text-gray-900 text-lg rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 p-3 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 h-11"  placeholder="Search" required="">
-            </div>
-        </form>
-    </div>
-</div>
-
-<!--End Table Header -->
-            <!--Table-->
-            <div class="overflow-x-auto">
-                <table class="w-full text-md font-medium text-left ">
-                    <thead class="text-md  bg-gray-50 dark:bg-gray-700 ">
-                        <tr>
-                            <th scope="col" class="px-4 py-3 text-md font-medium">Name</th>
-                            <th scope="col" class="px-4 py-3 text-md font-medium">Student ID</th>
-                            <th scope="col" class="px-4 py-3 text-md font-medium">College</th>
-                            <th scope="col" class="px-4 py-3 text-md font-medium">Course</th>
-                            <th scope="col" class="text-center px-4 py-3 text-md font-medium">Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    <tr class="border-b dark:border-gray-700">
-                            <th scope="row" class="px-4 py-3 text-gray-900 whitespace-nowrap dark:text-white text-md font-medium">Bryan The Dunker</th>
-                            <td class="px-4 py-3 text-md font-medium">201503664</td>
-                            <td class="px-4 py-3 text-md font-medium">CCS</td>
-                            <td class="px-4 py-3 text-md font-medium">Computer Science</td>
-                            <td class="px-4 py-3 text-md font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                <div class="flex justify-center items-center space-x-4">
-                                <button type="button" class="py-2 px-3 flex items-center justify-center text-sm font-medium text-center text-white bg-primary-700 rounded-lg hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-    onclick="window.location='{{ route('officer.paymentNext') }}'">
-    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-        <path fill-rule="evenodd"
-            d="M10 3a1 1 0 00-1 1v5H4a1 1 0 100 2h5v5a1 1 0 102 0v-5h5a1 1 0 100-2h-5V4a1 1 0 00-1-1z"
-            clip-rule="evenodd" />
-    </svg>
-</button>
-
-                        </div>
-                 </td>
-            </tr>
-        </table>
-</div>
+                <!-- End Table Header -->
+                <!-- Table -->
+                <div class="overflow-x-auto">
+                    <table class="w-full text-md font-medium text-left ">
+                        <thead class="text-md bg-gray-50 dark:bg-gray-700 ">
+                            <tr>
+                                <th scope="col" class="px-4 py-3 text-md font-medium">Name</th>
+                                <th scope="col" class="px-4 py-3 text-md font-medium">Student ID</th>
+                                <th scope="col" class="px-4 py-3 text-md font-medium">College</th>
+                                <th scope="col" class="px-4 py-3 text-md font-medium">Course</th>
+                                <th scope="col" class="text-center px-4 py-3 text-md font-medium">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr class="border-b dark:border-gray-700">
+                                <th scope="row" class="px-4 py-3 text-gray-900 whitespace-nowrap dark:text-white text-md font-medium">Bryan The Dunker</th>
+                                <td class="px-4 py-3 text-md font-medium">201503664</td>
+                                <td class="px-4 py-3 text-md font-medium">CCS</td>
+                                <td class="px-4 py-3 text-md font-medium">Computer Science</td>
+                                <td class="px-4 py-3 text-md font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    <div class="flex justify-center items-center space-x-4">
+                                        <button type="button" class="py-2 px-3 flex items-center justify-center text-sm font-medium text-center text-white bg-primary-700 rounded-lg hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800" onclick="window.location='{{ route('officer.paymentNext') }}'">
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                                <path fill-rule="evenodd" d="M10 3a1 1 0 00-1 1v5H4a1 1 0 100 2h5v5a1 1 0 102 0v-5h5a1 1 0 100-2h-5V4a1 1 0 00-1-1z" clip-rule="evenodd" />
+                                            </svg>
+                                        </button>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr class="border-b dark:border-gray-700">
+                                <th scope="row" class="px-4 py-3 text-gray-900 whitespace-nowrap dark:text-white text-md font-medium">Bryan The Dunker</th>
+                                <td class="px-4 py-3 text-md font-medium">201503664</td>
+                                <td class="px-4 py-3 text-md font-medium">CCS</td>
+                                <td class="px-4 py-3 text-md font-medium">Computer Science</td>
+                                <td class="px-4 py-3 text-md font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    <div class="flex justify-center items-center space-x-4">
+                                        <button type="button" class="py-2 px-3 flex items-center justify-center text-sm font-medium text-center text-white bg-primary-700 rounded-lg hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800" onclick="window.location='{{ route('officer.paymentNext') }}'">
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                                <path fill-rule="evenodd" d="M10 3a1 1 0 00-1 1v5H4a1 1 0 100 2h5v5a1 1 0 102 0v-5h5a1 1 0 100-2h-5V4a1 1 0 00-1-1z" clip-rule="evenodd" />
+                                            </svg>
+                                        </button>
+                                    </div>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             <!--End Table-->
             <!--Table Pagination-->
             <nav class="flex flex-col md:flex-row justify-between items-start md:items-center space-y-3 md:space-y-0 p-4" aria-label="Table navigation">
