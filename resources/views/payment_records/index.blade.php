22d48be3a3db6ef122ff34c4751a7480827adbd3
Yongbi21 <111903577+Yongbi21@users.noreply.github.com>
2024-03-14 01:03:48 +0800

    paymenr records

--- resources/views/payment_records/index.blade.php
+++ resources/views/payment_records/index.blade.php
@@ -103,7 +103,9 @@
                             <td class="px-4 py-3">2023-04-28 02:58:27</td>
                             <td class="px-4 py-3">USC644b1a3358b43</td>
                             <td class="px-4 py-3">Angelita the Officer</td>
-                            <td class="px-4 py-3">Paid</td>
+                            <td class="px-4 py-3">
+                                <span class="bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-md dark:bg-gray-700 dark:text-green-400 border border-green-100 dark:border-green-500">Paid</span>
+                            </td>
                             <td class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                 <div class="flex justify-center items-center space-x-4">
                                     <button type="button" class="py-2 px-3 flex items-center text-sm font-medium text-center text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
@@ -128,7 +130,9 @@
                             <td class="px-4 py-3">2023-04-28 02:58:27</td>
                             <td class="px-4 py-3">USC644b1a3358b43</td>
                             <td class="px-4 py-3">Angelita the Officer</td>
-                            <td class="px-4 py-3">Paid</td>
+                            <td class="px-4 py-3">
+                                <span class="bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-md dark:bg-gray-700 dark:text-green-400 border border-green-100 dark:border-green-500">Paid</span>
+                            </td>
                             <td class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                 <div class="flex justify-center items-center space-x-4">
                                     <button type="button" class="py-2 px-3 flex items-center text-sm font-medium text-center text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
