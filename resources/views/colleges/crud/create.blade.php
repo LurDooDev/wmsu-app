b84579696c6a5a3cf261c2c693b2bb35c552ec45
LurDooDev <111673346+LurDooDev@users.noreply.github.com>
2024-03-14 07:23:56 +0800

    Revert "Merge branch 'main' of https://github.com/LurDooDev/wmsu-app"
    This reverts commit cba273d3f9bbe6a786c4a80c7b084a446dcb5e04, reversing
    changes made to 152555ae7b32679bc708d7c2b45f54354b4811fe.

--- resources/views/colleges/crud/create.blade.php
+++ resources/views/colleges/crud/create.blade.php
@@ -53,58 +53,6 @@
         </div>
     </div>
 </div>
-{{-- preview modal --}}
-<div id="preview-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
-    <div class="relative p-4 w-full max-w-xl max-h-full">
-        <!-- Modal content -->
-        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-            <!-- Modal header -->
-            <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
-                <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
-                    View Departments
-                </h3>
-                <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="preview-modal">
-                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
-                    </svg>
-                    <span class="sr-only">Close modal</span>
-                </button>
-            </div>
-            <!-- Modal body -->
-            <form class="p-7 md:p-5" method="POST" action="">
-                @csrf
-                <div class="grid gap-4 mb-5 grid-cols-2">
-                    <div class="col-span-6">
-                        <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">College Name</label>
-                        <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
-                        focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400
-                        dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="College Of Computing Studies" required="" value="{{ old('name') }}"disabled>
-                        <span class="text-sm font-thin text-gray-500 dark:text-gray-400">Enter the full name of the college</span>
-
-                    </div>
-                    <div class="col-span-6">
-                        <label  for="college_code" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">College Code</label>
-                        <input type="text" name="college_code" id="college_code" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
-                        focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400
-                        dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="CCS" required="" value="{{ old('college_code') }}"disabled>
-                        <span class="text-sm font-thin text-gray-500 dark:text-gray-400">Enter the code for the college  </span>
-    
-                    </div>
-                </div>
-                <div id="courses-container">
-                    <!-- Dynamic Input for Courses -->
-                </div>
-                <div class="flex justify-between items-center mt-2">
-                    <button type="button" id="add-course-btn" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Add Course</button>
-
-                    <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
-                        Done
-                    </button>
-                </div>
-            </form>
-        </div>
-    </div>
-</div>
 
 <script type="module">
     $(document).ready(function () {
